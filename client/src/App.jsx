1a84fed28fd96dd56da270ed6f7b8393ae1c4f55
S7R4NGER3301 <nikolakoew@gmail.com>
2023-11-26 21:14:40 +0200

    adding header component html and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <h1>hi</h1>
+    <Header />
+     
     </>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className="header-area header-sticky">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="main-nav">
+              <a href="index.html" className="logo">
+                <img src="assets/images/logo.png" alt="" />
+              </a>
+              <div className="search-input">
+                <form id="search" action="#">
+                  <input
+                    type="text"
+                    placeholder="Type Something"
+                    id="searchText"
+                    name="searchKeyword"
+                  />
+                  <i className="fa fa-search" />
+                </form>
+              </div>
+              <ul className="nav">
+                <li>
+                  <a href="index.html" className="active">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="browse.html">Browse</a>
+                </li>
+                <li>
+                  <a href="details.html">Details</a>
+                </li>
+                <li>
+                  <a href="streams.html">Streams</a>
+                </li>
+                <li>
+                  <a href="profile.html">
+                    Profile{" "}
+                    <img src="assets/images/profile-header.jpg" alt="" />
+                  </a>
+                </li>
+              </ul>
+              <a className="menu-trigger">
+                <span>Menu</span>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
