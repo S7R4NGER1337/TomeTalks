a1e81e5b0be7fa9545da0440dba1dc2aa6f97cdb
S7R4NGER3301 <nikolakoew@gmail.com>
2023-12-10 15:28:35 +0200

    implement useFormHook on login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,27 @@ import Browse from "./components/Browse/Browse";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./components/Main/MainPage/MainPage";
 import Login from "./components/User/Login";
+import { useState } from "react";
+import AuthContext from "./contexts/authContext";
 
 function App() {
+  const [auth, setAuth] = useState({});
+
+  const loginSubmitHandler = (values) =>{
+    console.log(values);
+  }  
+
   return (
-    <>
+    <AuthContext.Provider>
       <Header />
 
       <Routes>
         <Route path="/" element={<MainPage />}/>
         <Route path="/books" element={<Browse />}/>
-        <Route path="/user/login" element={<Login />}/>
+        <Route path="/user/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>}/>
       </Routes>
 
-    </>
+    </AuthContext.Provider>
   );
 }
 
--- client/src/components/User/Login.jsx
+++ client/src/components/User/Login.jsx
@@ -1,21 +1,24 @@
 import './Login.css'
+import useForm from '../../hooks/useForm';
 
-export default function Login() {
+export default function Login({loginSubmitHandler}) {
+  const {values, onChange, onSubmit} = useForm(loginSubmitHandler, {email: '', password: ''})
+
   return (
     <div className="wrapper">
       <div className="inner">
-        <form action="" className='loginForm'>
+        <form action="" className='loginForm' onSubmit={onSubmit}>
           <h3>Login</h3>
           <p>
             Welcome to TomeTalks
           </p>
           <label className="form-group">
-            <input type="text" className="form-control" required="" />
-            <span>Your Name</span>
+            <input type="text" className="form-control" required="" name='email' onChange={onChange} value={values.email}/>
+            <span>Your Email</span>
             <span className="border" />
           </label>
           <label className="form-group">
-            <input type="password" className="form-control" required="" />
+            <input type="password" className="form-control" required="" name="password" onChange={onChange} value={values.password}/>
             <span htmlFor="">Your Password</span>
             <span className="border" />
           </label>
